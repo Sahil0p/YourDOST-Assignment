4a334ac91b5b54054aeb0bc25ab824e2f5ddeaba
Sahil Ahmed <sahilahmed0029@gmail.com>
2025-10-27 14:39:27 +0530

    first commit

--- dsa/second_largest_unique_number.js
+++ dsa/second_largest_unique_number.js
@@ -1,25 +1,37 @@
-function secondLargestUnique(arr) {
+function secondLargestUnique(arr) 
+{
     let first = -Infinity, second = -Infinity;
-    const seen = new Set();
+    const new_arr = new Set();
 
-    for (const num of arr) {
-        if (seen.has(num)) continue;
-        seen.add(num);
+    for (const num of arr) 
+    {
+        if (new_arr.has(num)) continue;
+        new_arr.add(num);
 
-        if (num > first) {
+        if (num > first) 
+        {
             second = first;
             first = num;
-        } else if (num > second && num < first) {
+        } 
+        else if (num > second && num < first) 
+        {
             second = num;
         }
     }
 
-    if (second === -Infinity) {
+    if (second === -Infinity) 
+    {
         return -1;
-    } else {
+    } 
+    else 
+    {
         return second;
     }
 }
 
 console.log(secondLargestUnique([3,5,2,5,6,6,1])); 
 console.log(secondLargestUnique([7, 7, 7]));
+console.log(secondLargestUnique([1]));
+console.log(secondLargestUnique([10, 5, 10]));
+console.log(secondLargestUnique([1, 3]));
+console.log(secondLargestUnique([]));
